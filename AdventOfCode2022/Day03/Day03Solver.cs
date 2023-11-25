904b334818de2014432f682f85e5104160a38179
a1ex9788 <alejandrocarrion1999@gmail.com>
2023-11-25 19:34:24 +0100

    Feat: Día 3.

--- AdventOfCode2022/Day03/Day03Solver.cs
+++ AdventOfCode2022/Day03/Day03Solver.cs
@@ -1,27 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2022.Day03
 {
     public class Day03Solver : IDaySolver
     {
-        private readonly IEnumerable<string> input;
+        private readonly IList<string> rucksacks;
 
         public Day03Solver(string input)
         {
-            this.input = input.Split("\r\n");
-
-            Console.Write(this.input.ToString());
+            this.rucksacks = input.Split("\r\n");
         }
 
         public long SolvePart1()
         {
-            return 0;
+            IList<(IList<char> A, IList<char> B)> parsedRucksacks =
+                new List<(IList<char> A, IList<char> B)>();
+
+            foreach (string rucksack in this.rucksacks)
+            {
+                int half = rucksack.Length / 2;
+
+                parsedRucksacks.Add(
+                    (rucksack[0..half].ToList(), rucksack[half..].ToList()));
+            }
+
+            int prioritiesSum = 0;
+
+            foreach ((IList<char> a, IList<char> b) in parsedRucksacks)
+            {
+                char commonItem = a.Intersect(b).Single();
+
+                prioritiesSum += Helper.GetPriority(commonItem);
+            }
+
+            return prioritiesSum;
         }
 
         public long SolvePart2()
         {
-            return 0;
+            int prioritiesSum = 0;
+
+            for (int i = 0; i < this.rucksacks.Count; i += 3)
+            {
+                string first = this.rucksacks[i];
+                string second = this.rucksacks[i + 1];
+                string third = this.rucksacks[i + 2];
+
+                char commonItem = first.Intersect(second).Intersect(third).Single();
+
+                prioritiesSum += Helper.GetPriority(commonItem);
+            }
+
+            return prioritiesSum;
         }
     }
 }
---
+++ AdventOfCode2022/Day03/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AdventOfCode2022.Day03
+{
+    internal static class Helper
+    {
+        internal static int GetPriority(char item)
+        {
+            int asciiValue = Convert.ToInt32(item);
+
+            if (char.IsLower(item))
+            {
+                return asciiValue - 96;
+            }
+
+            return asciiValue - 38;
+        }
+    }
+}
