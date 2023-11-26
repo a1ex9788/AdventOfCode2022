1ba0e7e2ab7b6e0cae8c7eccc6098fff4bd08520
a1ex9788 <alejandrocarrion1999@gmail.com>
2023-11-26 22:50:00 +0100

    Feat: Día 6.

--- AdventOfCode2022/Day06/Day06Solver.cs
+++ AdventOfCode2022/Day06/Day06Solver.cs
@@ -1,27 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2022.Day06
 {
     public class Day06Solver : IDaySolver
     {
-        private readonly IEnumerable<string> input;
+        private readonly IList<char> datastream;
 
         public Day06Solver(string input)
         {
-            this.input = input.Split("\r\n");
+            this.datastream = input.ToList();
 
-            Console.Write(this.input.ToString());
+            Console.Write(this.datastream.ToString());
         }
 
         public string SolvePart1()
         {
-            return string.Empty;
+            return this.GetStartOfMarker(4);
         }
 
         public string SolvePart2()
         {
-            return string.Empty;
+            return this.GetStartOfMarker(14);
+        }
+
+        private string GetStartOfMarker(int numberOfDistinctCharacters)
+        {
+            IList<char> differentCaracters = new List<char>();
+
+            for (int i = 0; i < this.datastream.Count; i++)
+            {
+                char currentChar = this.datastream[i];
+
+                while (differentCaracters.Contains(currentChar))
+                {
+                    differentCaracters.RemoveAt(0);
+                }
+
+                differentCaracters.Add(currentChar);
+
+                if (differentCaracters.Count == numberOfDistinctCharacters)
+                {
+                    return (i + 1).ToString();
+                }
+            }
+
+            throw new NotSupportedException();
         }
     }
 }
